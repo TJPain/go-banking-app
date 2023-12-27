50d79bc6be8f38b0bf72a081d7d7db5834dfdf65
Tom Pain <tom.pain@midnite.com>
2023-12-27 11:45:17 +0000

    Account update and delete methods

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,6 +7,12 @@ INSERT INTO accounts (
   $1, $2, $3
 ) RETURNING *;
 
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
 -- name: GetAccount :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
@@ -16,3 +22,7 @@ SELECT * FROM accounts
 ORDER BY id
 LIMIT $1
 OFFSET $2;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
