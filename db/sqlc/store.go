8e52801d8c40b9164edd419e3d23d95da93bd14b
Tom Pain <tom.pain@midnite.com>
2024-01-06 22:10:35 +0100

    DB transaction handling store

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -47,8 +47,51 @@ type TransferTxParams struct {
 	Amount        decimal.Decimal `json:"amount"`
 }
 
+// the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
 // TransferTX performs a money transfer from one account to another.
 // It creates a transfer record, adds account entries,and updates account balances within a single database transaction.
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
 
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    decimal.NewFromInt(1).Div(arg.Amount),
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO update account balances
+		return nil
+	})
+
+	return result, err
 }
