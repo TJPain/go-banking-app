e3d7672e8481d784d8a227d123e3928cbd4df3ef
Tom Pain <tom.pain@midnite.com>
2024-03-10 15:54:02 +0000

    Create mock db
    Feat: create mock db and store using mockgen

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,21 +7,27 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// Provides all functions to execute db queries and transactions
-type Store struct {
+// Store provides all functions to execute db queries and transactions
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
 		return err
@@ -58,7 +64,7 @@ type TransferTxResult struct {
 
 // TransferTX performs a money transfer from one account to another.
 // It creates a transfer record, adds account entries,and updates account balances within a single database transaction.
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
